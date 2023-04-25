d4edf5351d115cd51165e131899f2899f87e93f6
DorotaKiczor <dorokic980@student.polsl.pl>
2023-04-25 15:41:05 +0200

    Initial commit

---
+++ src/main/java/pl/zajecia/jenkinsHeroku/JenkinsHerokuApplication.java
@@ -0,0 +1,21 @@
+package pl.zajecia.jenkinsHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class JenkinsHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsHerokuApplication.class, args);
+	}
+
+
+	@GetMapping
+	public String get(){
+		return "Hello Jenkins!";
+	}
+}
